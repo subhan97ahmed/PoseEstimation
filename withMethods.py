8dc6f52786ad640800ed3a434f9ed777bc099a3d
talalali97 <talalali1397@gmail.com>
2022-04-23 03:20:53 +0500

    implemented curl exercise with counter (not working)

--- withMethods.py
+++ withMethods.py
@@ -433,17 +433,6 @@ def scoreExercise(name):
                                           mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
                                           mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
                                           )
-                # Plot Pose landmarks in 3D.
-                # mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
-
-                # for hands
-                # if resultshands.multi_hand_landmarks:
-                #     for handLms in resultshands.multi_hand_landmarks:
-                #         mp_drawing.draw_landmarks(image, handLms, mp_hand.HAND_CONNECTIONS,
-                #                                   mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2,
-                #                                                          circle_radius=2),
-                #                                   mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                #                                   )
 
                 cv2.imshow('Mediapipe Feed', image)
                 if cv2.waitKey(10) & 0xFF == ord('q'):
@@ -452,5 +441,113 @@ def scoreExercise(name):
             cap.release()
             cv2.destroyAllWindows()
 
-exerciseNames = ["wrist curl", "thumb flex", "squat"]
-scoreExercise(str(exerciseNames[2]))
+    elif name == "curl_counter":
+        # for pose
+        mp_drawing = mp.solutions.drawing_utils
+        mp_pose = mp.solutions.pose
+
+        # VIDEO FEED
+        cap = cv2.VideoCapture(0)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            cv2.imshow('Mediapipe Feed', frame)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        #Curl counter variables
+        counter = 0
+        stage = None
+
+        # Setup mediapipe instance
+        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+            while cap.isOpened():
+                ret, frame = cap.read()
+
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            # Make detection
+            results = pose.process(image)
+
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            #calculate angles
+            def calculate_angle(a, b, c):
+                a = np.array(a)
+                b = np.array(b)
+                c = np.array(c)
+
+                radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+                angle = np.abs(radians*180.0/np.pi)
+
+                if angle > 180.0:
+                    angle = 360-angle
+                return angle
+
+            #Extract Landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                # Get Coordinates
+                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+                # calculate angle
+                angle = calculate_angle(shoulder, elbow, wrist)
+
+                # visualize angle
+                cv2.putText(image, str(angle), tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AAA)
+
+                # Curl counter logic
+                if angle > 160:
+                    stage = "down"
+                if angle < 30 and stage == 'down':
+                    stage = "up"
+                    counter += 1
+                    print(counter)
+
+            except:
+                pass
+
+            #Render Curl Counter
+            #Setup status box
+            cv2.rectangle(image, (0, 0), (255, 73), (244, 117, 16), -1)
+
+            #Rep data
+            cv2.putText(image, 'REPS', (15, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter),
+                        (10, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Stage data
+            cv2.putText(image, 'STAGE', (65, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage,
+                        (60, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Render Detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
+
+            cv2.imshow("Mediapipe Feed", image)
+            # if cv2.waitKey(10) & 0xFF == ord('q'):
+            #     break
+            #
+            # cap.release()
+            # cv2.destroyAllWindows()
+
+
+exerciseNames = ["wrist curl", "thumb flex", "squat", "curl_counter"]
+scoreExercise(str(exerciseNames[3]))
