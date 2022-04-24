6005144bad075f64b7abd7636dfbdf5859f9e99e
subhan <subhan97ahmed@gmail.com>
2022-04-24 17:12:59 +0500

    fixed arm curls and its counter

--- withMethods.py
+++ withMethods.py
@@ -441,21 +441,14 @@ def scoreExercise(name):
             cap.release()
             cv2.destroyAllWindows()
 
-    elif name == "curl_counter":
+    elif name == "arm curl":
         # for pose
         mp_drawing = mp.solutions.drawing_utils
         mp_pose = mp.solutions.pose
 
-        # VIDEO FEED
         cap = cv2.VideoCapture(0)
-        while cap.isOpened():
-            ret, frame = cap.read()
-            cv2.imshow('Mediapipe Feed', frame)
 
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-
-        #Curl counter variables
+        # Curl counter variables
         counter = 0
         stage = None
 
@@ -464,90 +457,78 @@ def scoreExercise(name):
             while cap.isOpened():
                 ret, frame = cap.read()
 
-            # Recolor image to RGB
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image.flags.writeable = False
+                # Recolor image to RGB
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
 
-            # Make detection
-            results = pose.process(image)
+                # Make detection
+                results = pose.process(image)
 
-            # Recolor back to BGR
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                # Recolor back to BGR
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            #calculate angles
-            def calculate_angle(a, b, c):
-                a = np.array(a)
-                b = np.array(b)
-                c = np.array(c)
+                # Extract Landmarks
+                try:
+                    landmarks = results.pose_landmarks.landmark
 
-                radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-                angle = np.abs(radians*180.0/np.pi)
+                    # Get Coordinates
+                    l_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                                  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                    l_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                               landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                    l_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                               landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
 
-                if angle > 180.0:
-                    angle = 360-angle
-                return angle
+                    # calculate angle
+                    angle = calculate_angle(l_shoulder, l_elbow, l_wrist)
 
-            #Extract Landmarks
-            try:
-                landmarks = results.pose_landmarks.landmark
+                    # visualize angle
 
-                # Get Coordinates
-                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                         landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                    cv2.putText(image, str(angle), tuple(np.multiply(l_elbow, [640, 480]).astype(int)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                    print(str(angle))
+                    print(type(angle))
+                    # Curl counter logic
+                    if angle > 150.0:
+                        stage = "down"
+                    if angle < 40.0 and stage == 'down':
+                        stage = "up"
+                        counter += 1
+                        print(counter)
 
-                # calculate angle
-                angle = calculate_angle(shoulder, elbow, wrist)
+                except:
+                    pass
 
-                # visualize angle
-                cv2.putText(image, str(angle), tuple(np.multiply(elbow, [640, 480]).astype(int)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AAA)
+                # Render Curl Counter
+                # Setup status box
+                cv2.rectangle(image, (0, 0), (255, 73), (244, 117, 16), -1)
 
-                # Curl counter logic
-                if angle > 160:
-                    stage = "down"
-                if angle < 30 and stage == 'down':
-                    stage = "up"
-                    counter += 1
-                    print(counter)
+                # Rep data
+                cv2.putText(image, 'REPS', (15, 12),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+                cv2.putText(image, str(counter),
+                            (10, 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
-            except:
-                pass
+                # Stage data
+                cv2.putText(image, 'STAGE', (65, 12),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+                cv2.putText(image, str(stage),
+                            (60, 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
-            #Render Curl Counter
-            #Setup status box
-            cv2.rectangle(image, (0, 0), (255, 73), (244, 117, 16), -1)
+                # Render Detections
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                          mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                          mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
 
-            #Rep data
-            cv2.putText(image, 'REPS', (15, 12),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-            cv2.putText(image, str(counter),
-                        (10, 60),
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-            # Stage data
-            cv2.putText(image, 'STAGE', (65, 12),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-            cv2.putText(image, stage,
-                        (60, 60),
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-            # Render Detections
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
-
-            cv2.imshow("Mediapipe Feed", image)
-            # if cv2.waitKey(10) & 0xFF == ord('q'):
-            #     break
-            #
-            # cap.release()
-            # cv2.destroyAllWindows()
+                cv2.imshow("Mediapipe Feed", image)
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+            cap.release()
+            cv2.destroyAllWindows()
 
 
-exerciseNames = ["wrist curl", "thumb flex", "squat", "curl_counter"]
+exerciseNames = ["wrist curl", "thumb flex", "squat", "arm curl"]
 scoreExercise(str(exerciseNames[3]))
