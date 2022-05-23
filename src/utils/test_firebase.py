1711076d6b63b0bdb7e5e9de80ea22ecfbf01235
subhan <subhan97ahmed@gmail.com>
2022-05-23 23:40:38 +0500

    initialize firebase

---
+++ src/utils/test_firebase.py
@@ -0,0 +1,42 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate('../json/posefect-firebase-adminsdk.json')
+firebase_admin.initialize_app(cred,
+                              {
+                                  'databaseURL': 'https://posefect-b48b9-default-rtdb.firebaseio.com/'
+                              })
+
+
+def add_data_firebase(foodName):
+    # import pandas as pd
+    # import firebase_admin
+    # from firebase_admin import credentials, firestore
+    # cred = credentials.Certificate('./ai-kitchen-helper-firebase-adminsdk.json')
+    # firebase_admin.initialize_app(cred,
+    #                               {
+    #                                   'databaseURL': 'https://ai-kitchen-helper.firebaseio.com/'
+    #                               })
+    db = firestore.client()
+    doc_ref = db.collection(u'test')
+    # Import data
+    # df = pd.read_csv(‘PPP_data.csv’)
+    # tmp = df.to_dict(orient='records')
+    # list(map(lambda x: doc_ref.add(x), 'tmp'))
+
+    # item = Item(name=u'lemon', present=True)
+    # doc_ref.add(item.to_dict())
+
+    data = {
+        u'name': u'' + foodName,
+        u'present': True
+    }
+    # TODO change it to uuid
+    # i = random.randint(1, 1000)
+    db.collection(u'test').document(u'user_1').set(data)
+    docs = doc_ref.stream()
+    for doc in docs:
+        print(f'{doc.id} = > {doc.to_dict()}')
+
+
+add_data_firebase('hello')
