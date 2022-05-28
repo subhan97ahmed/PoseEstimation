142c2ae4d94c4a948eb8306c0cb225696e163a00
Mohammad Anas <Mohammad.anas190@gmail.com>
2022-05-28 16:16:55 +0500

    update requirements file add youtube video test add center widget function

--- src/utils/util.py
+++ src/utils/util.py
@@ -1,6 +1,10 @@
 # Add utility code here that will be shared across the application
 # We can add our functions here for fetching and transforming data
 # through that we can share those functions for both user types
+from PyQt5.QtWidgets import QDesktopWidget
 
-def some_func():
-    print('Im working yay!')
+def center(self):
+    qr = self.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    self.move(qr.topLeft())
