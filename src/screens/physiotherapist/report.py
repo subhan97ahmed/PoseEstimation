b3062f8614eb6b25b49158fd8573a532f8fbe18b
subhan <subhan97ahmed@gmail.com>
2022-06-01 13:57:23 +0500

    add graphs in patient and therapist report

--- src/screens/physiotherapist/report.py
+++ src/screens/physiotherapist/report.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication
 from src.ui.TherapistReportView import Ui_TherapistReport
-
+import pyqtgraph as pg
 
 class TReport(QWidget, Ui_TherapistReport):
     def __init__(self, parent=None):
@@ -12,6 +12,23 @@ class TReport(QWidget, Ui_TherapistReport):
         self.HomeButton.clicked.connect(self.parent().go_to_0)
         self.TreatmentButton.clicked.connect(self.parent().go_to_3)
         self.ReportButton.clicked.connect(self.parent().go_to_1)
+
+
+        # this data will come from firebase
+        days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        score = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
+        self.graphWidget = pg.PlotWidget()
+        self.scrollArea.setWidget(self.graphWidget)
+        self.graphWidget.setBackground('w')
+
+        self.graphWidget.setLabel('left', "<span style=\"color:blue;font-size:20px\">Score (%)</span>")
+        self.graphWidget.setLabel('bottom', "<span style=\"color:blue;font-size:20px\">Time (Days)</span>")
+
+        pen = pg.mkPen(color=(0, 0, 220), width=5)
+
+        # plot data: x, y values
+        self.graphWidget.plot(days, score, pen=pen, symbol='o', symbolSize=15, symbolBrush=('b'))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
