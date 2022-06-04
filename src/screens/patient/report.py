7cdc149cbfff16dc45f4055afb6b135f32dc9c59
subhan <subhan97ahmed@gmail.com>
2022-06-04 14:09:41 +0500

    fix report in therapist

--- src/screens/patient/report.py
+++ src/screens/patient/report.py
@@ -4,6 +4,8 @@ from src.ui.TherapistReportView import Ui_TherapistReport
 
 import pyqtgraph as pg
 
+from src.utils.util import get_age
+
 
 class TReport(QWidget, Ui_TherapistReport):
     def __init__(self, parent=None):
@@ -15,8 +17,21 @@ class TReport(QWidget, Ui_TherapistReport):
         self.HomeButton.clicked.connect(self.parent().go_to_0)
         self.TreatmentButton.clicked.connect(self.parent().go_to_1)
         # self.ReportButton.clicked.connect(self.parent().go_to_3)
-#         static graph
-
+        info = self.parent().user_info
+        # db = firestore.client()
+        # fb = db.collection(u'users').document(u'' + info['uId'])
+        # print("fv: ", fb)
+        print(info)
+        self.UsernameLabel_2.setText(info["f_name"])
+        self.patientName_3.setText(info["f_name"])
+        self.patientEmail_4.setText(info["email"])
+        self.PatientContactNo_4.setText('-')
+        self.patientAge_3.setText(str(get_age(info["dob"])))
+        #
+        if 'diagnosis_1' in info.keys():
+            self.disease_4.setText(info["diagnosis_1"])
+            self.disease_5.setText(info["diagnosis_2"])
+            self.disease_6.setText(info["diagnosis_3"])
         # this data will come from firebase
         days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         score = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
@@ -31,6 +46,7 @@ class TReport(QWidget, Ui_TherapistReport):
 
         # plot data: x, y values
         self.graphWidget.plot(days, score, pen=pen, symbol='o', symbolSize=15, symbolBrush=('b'))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
