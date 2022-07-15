befaf5355cabd07feb96f0d9406164b783d8ac77
subhan <subhan97ahmed@gmail.com>
2022-07-16 02:29:48 +0500

    completed exercise card functionality and add it to view_exercises

--- src/screens/patient/view_exercises.py
+++ src/screens/patient/view_exercises.py
@@ -13,12 +13,27 @@ class PExercisePrescribe(QWidget, Ui_ExercisePrescribe):
         self.HomeButton.clicked.connect(self.parent().go_to_0)
         # self.TreatmentButton.clicked.connect(self.parent().go_to_1)
         self.ReportButton.clicked.connect(self.parent().go_to_3)
-        self.StartExerciseBtn.clicked.connect(self.parent().go_to_2)
-        ExerciseCard(
-            frame=self.frame_3
-            # patient_info=doc_data,
-            # event_func=lambda x: self.onAddTreatment(x),
-        )
+        # self.StartExerciseBtn.clicked.connect(self.parent().go_to_2)
+        self.exercises = self.parent().user_info["assigned_ex"]
+        exercise_index = 0
+        exercise_card = {}
+        for exercise in self.exercises:
+            exercise_card[exercise_index] = ExerciseCard(
+                frame=self.frame_3,
+                exercise_info=exercise,
+                event_func=lambda x: self.onStartExercise(x),
+            )
+            if exercise_index != 0:
+                exercise_card[exercise_index].ExerciseCard.move(
+                    exercise_card[exercise_index - 1].ExerciseCard.rect().x() + exercise_card[
+                        exercise_index - 1].ExerciseCard.rect().width() + 15,
+                    exercise_card[exercise_index - 1].ExerciseCard.rect().y())
+            exercise_index = exercise_index + 1
+
+    def onStartExercise(self, exercise_info):
+        self.parent().parent().set_hold_data(exercise_info)
+        print(f"on start exercise: ", exercise_info)
+        self.parent().parent().go_to_2()
 
 
 if __name__ == "__main__":
