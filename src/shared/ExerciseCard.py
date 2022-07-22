11570bdf14696035d105ffacfc259b9e4d3c3f14
Mohammad Anas <Mohammad.anas190@gmail.com>
2022-07-23 03:01:49 +0500

    feat: add patient report card fix exercise card geometry

--- src/shared/ExerciseCard.py
+++ src/shared/ExerciseCard.py
@@ -2,14 +2,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import src.resource.fonts_rc
 import src.resource.icons_rc
 import src.resource.images_rc
-from src.utils.util import get_age
 
 
 class ExerciseCard:
-    def __init__(self, frame,exercise_info, event_func=any):
+    def __init__(self, frame, exercise_info, event_func=any):
         _translate = QtCore.QCoreApplication.translate
         self.ExerciseCard = QtWidgets.QWidget(frame)
-        self.ExerciseCard.setGeometry(QtCore.QRect(0, -10, 331, 151))
+        self.ExerciseCard.setGeometry(QtCore.QRect(0, 0, 331, 151))
         self.ExerciseCard.setStyleSheet("background-color: #f0f4c3;\n"
                                         "border-radius: 10px;")
         self.ExerciseCard.setObjectName("ExerciseCard")
@@ -35,77 +34,5 @@ class ExerciseCard:
         self.StartExerciseBtn.setObjectName("StartExerciseBtn")
         # set values
         self.ExerciseName.setText(_translate("ExercisePrescribe", str(exercise_info["name"]).upper()))
-        self.RepCount.setText(_translate("ExercisePrescribe", "Rep "+str(exercise_info["rep_count"])))
+        self.RepCount.setText(_translate("ExercisePrescribe", "Rep " + str(exercise_info["rep_count"])))
         self.StartExerciseBtn.clicked.connect(lambda: event_func(exercise_info))
-
-        #
-        # self.ExerciseCard = QtWidgets.QWidget(frame)
-        # self.ExerciseCard.setGeometry(QtCore.QRect(0, 0, 311, 311))
-        # self.ExerciseCard.setStyleSheet("border-radius: 10px;\n"
-        #                                "background-color: #f0f4c3;\n"
-        #                                "")
-        # self.ExerciseCard.setObjectName("ExerciseCard")
-        # self.label = QtWidgets.QLabel(self.ExerciseCard)
-        # self.label.setGeometry(QtCore.QRect(10, 20, 61, 51))
-        # self.label.setStyleSheet("background-color: transparent;")
-        # self.label.setText("")
-        # self.label.setTextFormat(QtCore.Qt.RichText)
-        # self.label.setPixmap(QtGui.QPixmap(":/iconPrefix/user.png"))
-        # self.label.setObjectName("label")
-        # self.Patient_name = QtWidgets.QLabel(self.ExerciseCard)
-        # self.Patient_name.setGeometry(QtCore.QRect(80, 40, 211, 21))
-        # self.Patient_name.setStyleSheet("background-color: transparent;\n"
-        #                                 "font-size: 16px;")
-        # self.Patient_name.setObjectName("Patient_name")
-        # self.Patient_age = QtWidgets.QLabel(self.ExerciseCard)
-        # self.Patient_age.setGeometry(QtCore.QRect(80, 70, 211, 21))
-        # self.Patient_age.setStyleSheet("background-color: transparent;\n"
-        #                                "font-size: 14px;")
-        # self.Patient_age.setObjectName("Patient_age")
-        # self.disease_1 = QtWidgets.QLabel(self.ExerciseCard)
-        # self.disease_1.setGeometry(QtCore.QRect(10, 100, 291, 31))
-        # self.disease_1.setStyleSheet("border-radius: 15px;\n"
-        #                              "background-color: #fb8c00;\n"
-        #                              "font-size: 12px;")
-        # self.disease_1.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft)
-        # self.disease_1.setObjectName("disease_1")
-        # self.TreatmentBtn = QtWidgets.QPushButton(self.ExerciseCard)
-        # self.TreatmentBtn.setGeometry(QtCore.QRect(90, 260, 131, 41))
-        # self.TreatmentBtn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        # self.TreatmentBtn.setStyleSheet("margin: 0;\n"
-        #                                 "background-color: #fb8c00;\n"
-        #                                 "border-radius: 20px;")
-        # self.TreatmentBtn.setObjectName("TreatmentBtn")
-        # self.notificationBtn = QtWidgets.QCommandLinkButton(self.ExerciseCard)
-        # self.notificationBtn.setGeometry(QtCore.QRect(270, 0, 31, 31))
-        # self.notificationBtn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        # self.notificationBtn.setText("")
-        # icon5 = QtGui.QIcon()
-        # icon5.addPixmap(QtGui.QPixmap(":/iconPrefix/notification.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        # self.notificationBtn.setIcon(icon5)
-        # self.notificationBtn.setObjectName("notificationBtn")
-        # self.disease_2 = QtWidgets.QLabel(self.ExerciseCard)
-        # self.disease_2.setGeometry(QtCore.QRect(10, 150, 291, 31))
-        # self.disease_2.setStyleSheet("border-radius: 15px;\n"
-        #                              "background-color: #fb8c00;\n"
-        #                              "font-size: 12px;")
-        # self.disease_2.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft)
-        # self.disease_2.setObjectName("disease_2")
-        # self.disease_3 = QtWidgets.QLabel(self.ExerciseCard)
-        # self.disease_3.setGeometry(QtCore.QRect(10, 200, 291, 31))
-        # self.disease_3.setStyleSheet("border-radius: 15px;\n"
-        #                              "background-color: #fb8c00;\n"
-        #                              "font-size: 12px;")
-        # self.disease_3.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft)
-        # self.disease_3.setObjectName("disease_3")
-        #
-        # # Set values
-        # self.Patient_name.setText(_translate("Treatment", exercise_info["f_name"]))
-        # self.Patient_age.setText(_translate("Treatment", get_age(exercise_info["dob"])))
-        # self.disease_1.setText(_translate("Treatment", patient_info["diagnosis_1"]))
-        # self.disease_2.setText(_translate("Treatment", patient_info["diagnosis_2"]))
-        # self.disease_3.setText(_translate("Treatment", patient_info["diagnosis_3"]))
-        # self.TreatmentBtn.setText(_translate("Treatment", "Add Treatment"))
-        # self.TreatmentBtn.clicked.connect(lambda: event_func(patient_info))
-
-
---
+++ src/shared/PatientReportCard.py
@@ -0,0 +1,61 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import src.resource.fonts_rc
+import src.resource.icons_rc
+import src.resource.images_rc
+from src.utils.util import get_age
+
+
+class PatientReportCard:
+    def __init__(self, frame, patient, event_func=any):
+        _translate = QtCore.QCoreApplication.translate
+        self.PatientNameCard = QtWidgets.QWidget(frame)
+        self.PatientNameCard.setGeometry(QtCore.QRect(0, 0, 301, 201))
+        self.PatientNameCard.setStyleSheet("border-radius: 10px;\n"
+                                           "background-color: #f0f4c3;\n"
+                                           "\n"
+                                           "")
+        self.PatientNameCard.setObjectName("PatientNameCard")
+        self.label_4 = QtWidgets.QLabel(self.PatientNameCard)
+        self.label_4.setGeometry(QtCore.QRect(10, 30, 61, 51))
+        self.label_4.setStyleSheet("background-color: transparent;\n"
+                                   "width: 20px;\n"
+                                   "height: auto")
+        self.label_4.setText("")
+        self.label_4.setTextFormat(QtCore.Qt.RichText)
+        self.label_4.setPixmap(QtGui.QPixmap(":/iconPrefix/treatment.png"))
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_4.setWordWrap(False)
+        self.label_4.setObjectName("label_4")
+        self.patientNameLabel = QtWidgets.QLabel(self.PatientNameCard)
+        self.patientNameLabel.setGeometry(QtCore.QRect(80, 20, 211, 21))
+        self.patientNameLabel.setStyleSheet("background-color: transparent;\n"
+                                            "font-size: 16px;")
+        self.patientNameLabel.setObjectName("patientNameLabel")
+        self.patientAge = QtWidgets.QLabel(self.PatientNameCard)
+        self.patientAge.setGeometry(QtCore.QRect(80, 110, 211, 21))
+        self.patientAge.setStyleSheet("background-color: transparent;\n"
+                                      "font-size: 14px;")
+        self.patientAge.setObjectName("patientAge")
+        self.patientEmail = QtWidgets.QLabel(self.PatientNameCard)
+        self.patientEmail.setGeometry(QtCore.QRect(80, 50, 211, 21))
+        self.patientEmail.setStyleSheet("background-color: transparent;\n"
+                                        "font-size: 14px;")
+        self.patientEmail.setObjectName("patientEmail")
+        self.patientContactNo = QtWidgets.QLabel(self.PatientNameCard)
+        self.patientContactNo.setGeometry(QtCore.QRect(80, 80, 211, 21))
+        self.patientContactNo.setStyleSheet("background-color: transparent;\n"
+                                            "font-size: 14px;")
+        self.patientContactNo.setObjectName("patientContactNo")
+        self.addPatientBtn = QtWidgets.QPushButton(self.PatientNameCard)
+        self.addPatientBtn.setGeometry(QtCore.QRect(140, 140, 151, 41))
+        self.addPatientBtn.setStyleSheet("background-color: rgb(249, 168, 37);\n"
+                                         "border-radius: 20px;")
+        self.addPatientBtn.setObjectName("addPatientBtn")
+
+        # Set values
+        self.patientNameLabel.setText(_translate("TherapistReports", f"{patient['rep_count']}"))
+        self.patientAge.setText(_translate("TherapistReports", f"{get_age(patient['dob'])}"))
+        self.patientEmail.setText(_translate("TherapistReports", f"{patient['email']}"))
+        self.patientContactNo.setText(_translate("TherapistReports", "---"))
+        self.addPatientBtn.setText(_translate("TherapistReports", "View Report"))
+        self.addPatientBtn.clicked.connect(lambda: event_func(patient))
