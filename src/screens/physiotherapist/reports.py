d67d4c06be9a87457536d1b38c68c00bc2986018
talalali97 <talalali1397@gmail.com>
2022-07-23 03:32:04 +0500

    generating patient_name_card in reports

--- src/screens/physiotherapist/reports.py
+++ src/screens/physiotherapist/reports.py
@@ -1,6 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication
 from src.ui.TherapistReportsView import Ui_TherapistReports
+from firebase_admin import firestore
+from src.shared.PatientReportCard import PatientReportCard
+
 
 
 class TReports(QWidget, Ui_TherapistReports):
@@ -14,6 +17,39 @@ class TReports(QWidget, Ui_TherapistReports):
         self.TreatmentButton.clicked.connect(self.parent().go_to_3)
         self.ReportButton.clicked.connect(self.parent().go_to_1)
         self.addPatientBtn.clicked.connect(self.parent().go_to_2)
+        self.patients = self.parent().user_info["assigned_patients"]
+        if not len(self.patients):
+            return
+        db = firestore.client()
+        patient_index = 0
+        patient_name_card = {}
+        for patient in self.patients:
+            print("patient: ", patient)
+            doc_ref = db.collection(u'users').document(u'' + patient)
+            doc = doc_ref.get()
+            if doc.exists:
+                doc_data = doc.to_dict()
+                print("============doc_data: ", doc_data)
+
+                patient_name_card[patient_index] = PatientReportCard(
+                    frame=self.scrollHolder,
+                    patient=doc_data,
+                    event_func=lambda x: self.onView(x),
+                )
+
+                self.hbox.insertWidget(patient_index,patient_name_card[patient_index].PatientNameCard)
+
+                if patient_index != 0:
+                    patient_name_card[patient_index].PatientNameCard.move(
+                        patient_name_card[patient_index - 1].PatientNameCard.rect().x() + patient_name_card[
+                            patient_index - 1].PatientNameCard.rect().width() + 15,
+                        patient_name_card[patient_index - 1].PatientNameCard.rect().y())
+                patient_index = patient_index + 1
+
+    def onView(self, patient):
+        self.parent().parent().set_hold_data(patient)
+        print(f"on view: ", patient)
+        self.parent().parent().go_to_2()
 
 
 
