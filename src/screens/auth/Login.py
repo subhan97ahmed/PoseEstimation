fa486a9d3833ddcdbeef3f3bc323b7635a464017
Mohammad Anas <Mohammad.anas190@gmail.com>
2022-07-28 02:01:02 +0500

    feat: remove addon style and static login

--- src/screens/auth/Login.py
+++ src/screens/auth/Login.py
@@ -2,13 +2,7 @@ import sys
 from PyQt5.QtWidgets import QWidget, QApplication
 from src.ui.LoginView import Ui_Login
 from src.utils.util import is_form_empty
-from src.utils.static import addonStyles
 
-#todo: remove this after work
-static_login_info = {
-            "email": 'posetest@yopmail.com',
-            "password": '123456',
-        }
 
 class Login(QWidget, Ui_Login):
     def __init__(self, parent=None):
@@ -18,7 +12,7 @@ class Login(QWidget, Ui_Login):
         # button click method for screen change
         self.CreateAnAcc.clicked.connect(self.parent().go_to_1)
         self.loginBtn.clicked.connect(self.submit_login)
-        self.setStyleSheet(self.styleSheet() + addonStyles)
+        self.setStyleSheet(self.styleSheet())
 
     def submit_login(self):
         login_info = {
